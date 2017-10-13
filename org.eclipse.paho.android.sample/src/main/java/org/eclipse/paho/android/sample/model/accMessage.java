1a9eca909588a106968d7719eb989285e3319e70
siyi.zheng <siyi.zheng@aalto.fi>
2017-10-13 13:55:59 +0300

    add acc mesg

--- org.eclipse.paho.android.sample/src/main/java/org/eclipse/paho/android/sample/model/accMessage.java
+++ org.eclipse.paho.android.sample/src/main/java/org/eclipse/paho/android/sample/model/accMessage.java
@@ -5,4 +5,12 @@ package org.eclipse.paho.android.sample.model;
  */
 
 public class accMessage {
+
+    public String timestamp;
+    public String accelerometer;
+
+    public accMessage(String time, String value) {
+        this.timestamp = time;
+        this.accelerometer = value;
+    }
 }
